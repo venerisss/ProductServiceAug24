77d3f4b17849add66fefa41baced484bf9b23595
ishaque <mishafi1234@gmail.com>
2024-10-02 21:50:35 +0530

    added spring data jpa to project

--- src/main/java/com/example/ProductServiceAug24/Models/Product.java
+++ src/main/java/com/example/ProductServiceAug24/Models/Product.java
@@ -1,10 +1,15 @@
 package com.example.ProductServiceAug24.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 /*@Getter //Using this annotations, all the attributes will have getters(using LOMBOK)
 @Setter*/
+@Entity //this will help springboot understand that we need to add the following as tables to the db
 public class Product {
+    @Id //this is used to tell spring boot that this is the attribute that should be taken as the primary key
+    private long id;
     String name;
     String category;
     String description; //use cmd n to select getter and setter
@@ -31,5 +36,13 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
     }
 }
